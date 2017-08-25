9738f363c27f3e8c2d5da02ec0a73bdf65565999
Anson Li <incendie323@gmail.com>
2017-08-25 16:15:27 -0400

    added ability to pick random Movie+Anime from filtered list

--- scripts/script.js
+++ scripts/script.js
@@ -10,12 +10,6 @@ var recommendApp = {};
 	recommendApp.maxShows = 41;
 	recommendApp.maxAnime = 14;
 
-	//initialize arrays to save data
-	recommendApp.movies = {};
-	recommendApp.shows = {};
-	recommendApp.anime = {};
-	// recommendApp.userGenres = [];
-
 //AJAX method for PopcornTime
 recommendApp.requestPopcorn = (endpoint) => {
 	var request = $.ajax({
@@ -80,6 +74,8 @@ recommendApp.getUserGenres = () => {
 		e.preventDefault();
 		let themes = [];
 		let userInput = $(`input[name="userGenres"]:checked`);
+		
+		//limit to max 3 genres
 		if (userInput.length <=3){
 			userInput.each((i,el) => {
 				themes.push(el.value);
@@ -89,8 +85,10 @@ recommendApp.getUserGenres = () => {
 			alert("Please only check 3 or less genres");
 		}
 		recommendApp.userGenres = themes;
-		console.log("Genres",recommendApp.userGenres);
-		recommendApp.getRecommendations();
+		console.log("User's genres",recommendApp.userGenres);
+
+		//pass movies, shows, anime, user genres, user time
+		recommendApp.getRecommendations(recommendApp.movies, recommendApp.shows, recommendApp.anime, recommendApp.userGenres, recommendApp.userTime);
 	});
 };
 
@@ -103,6 +101,7 @@ recommendApp.getMovies = () => {
 		$.when(data).then( ()=>{
 			let array = data.responseJSON;
 
+			//saving the relevant datas
 			for (let n=0; n<array.length; n++) {
 				let movie = array[n]
 				// console.log(movie);
@@ -144,66 +143,168 @@ recommendApp.getShows = () => {
 //grab all anime from Popcorn Time and save relevant data
 recommendApp.getAnime = () => {
 	//need: title, type(show/movie), genres(array), images.poster, num_seasons(if available)
-	for(let i=1; i<=recommendApp.maxMovies;i++){
+	for(let i=1; i<=recommendApp.maxAnime;i++){
 		let data = recommendApp.requestPopcorn(`animes/${i}`);
 		
 		$.when(data).then( ()=>{
 			let array = data.responseJSON;
 
 			for (let n=0; n<array.length; n++) {
-				let movie = array[n]
+				let series = array[n]
 				// console.log(movie);
-				recommendApp.anime[movie.title] = {};
+				recommendApp.anime[series.title] = {};
 				// recommendApp.anime[movie.title].genres = [];
-				recommendApp.anime[movie.title].seasons = parseInt(movie.num_seasons);
-				recommendApp.anime[movie.title].title = movie.title;
-				recommendApp.anime[movie.title].poster = movie.images.poster;
-				recommendApp.anime[movie.title].genres = movie.genres
+				recommendApp.anime[series.title].seasons = parseInt(series.num_seasons);
+				recommendApp.anime[series.title].title = series.title;
+				recommendApp.anime[series.title].poster = series.images.poster;
+				recommendApp.anime[series.title].genres = series.genres
+			}
+
+			for (let series in recommendApp.anime) {
+				for (let n=0; n<recommendApp.anime[series].genres.length; n++) {
+					recommendApp.anime[series].genres[n] = recommendApp.anime[series].genres[n].toLowerCase();
+					if (recommendApp.anime[series].genres[n] == "sci-fi") {
+						recommendApp.anime[series].genres[n] = "science-fiction";
+					}
+				}
 			}
 		});
 	}
 };
 
-recommendApp.getRecommendations = () => {
-	let uGenres = recommendApp.userGenres;
-	let movies = recommendApp.movies;
+//container for getting all recommendations
+recommendApp.getRecommendations = (movies, shows, anime, uGenres, uTime) => {
 
-	for (let movie in movies) {
-		let matches = 0;
-		for (let i=0; i<uGenres.length; i++){
-			for (let n=0; n<movies[movie].genres.length; n++) {
-				if (uGenres[i] === movies[movie].genres[n]){
-					matches++;
-				}
-			}
-			if (matches>0) {
-				console.log(movies[movie].title);
-			}
-		}
-	}
+	recommendApp.getMovieRecs(movies, uGenres, uTime);
+	recommendApp.getAnimeRecs(anime, uGenres);
+	recommendApp.displayBest();
 };
 
+recommendApp.displayBest = () => {
+	//display only the best matches possible
+	for (let i=2; i>0; i--){
+		if (recommendApp.recMovies[i].length) {
+			var randomMovie = recommendApp.recMovies[i][Math.floor(Math.random()*recommendApp.recMovies.length)];
+			break;
+		}
+	}
+	console.log(randomMovie);
+
+	for (let i=2; i>0; i--){
+		if (recommendApp.recAnime[i].length) {
+			var randomAnime = recommendApp.recAnime[i][Math.floor(Math.random()*recommendApp.recAnime.length)];
+			break;
+		}
+	}
+	console.log(randomAnime);
+}
+
+//get movie recommendations
+recommendApp.getMovieRecs = (movies, uGenres, uTime) => {
+	//finding out how well each movie matches
+	for (let movie in movies) {
+		let matches = 0;
+
+		//must fit within user's timeframe
+		if (movies[movie].runtime <= uTime){
+			//checking for how well genres match
+			for (let i=0; i<uGenres.length; i++){
+				for (let n=0; n<movies[movie].genres.length; n++) {
+					if (uGenres[i] === movies[movie].genres[n]){
+						matches++;
+					}
+				}//for movie.genres
+			}//for uGenres
+		}//if
+
+		//breaking up movies to how close they match the user
+		if (matches === 3) {
+			recommendApp.recMovies[2].push(movies[movie].title);
+		}
+		else if (matches === 2) {
+			recommendApp.recMovies[1].push(movies[movie].title);
+		}
+		else if (matches === 1) {
+			recommendApp.recMovies[0].push(movies[movie].title);
+		}
+	}
+	console.log("Recommended Movies:",recommendApp.recMovies);
+}
+
+//container for getting user input
 recommendApp.getUserInput = () => {
 	recommendApp.getUserTime();
 	recommendApp.getUserGenres();
+};
+
+//get anime recommendations
+recommendApp.getAnimeRecs = (anime, uGenres) => {
+	//finding out how well each movie matches
+	for (let series in anime) {
+		let matches = 0;
+
+		//checking for how well genres match
+		for (let i=0; i<uGenres.length; i++){
+			for (let n=0; n<anime[series].genres.length; n++) {
+				if (uGenres[i] === anime[series].genres[n]){
+					matches++;
+				}
+			}//for anime.genres
+		}//for uGenres
+
+
+		//breaking up movies to how close they match the user
+		if (matches === 3) {
+			recommendApp.recAnime[2].push(anime[series].title);
+		}
+		else if (matches === 2) {
+			recommendApp.recAnime[1].push(anime[series].title);
+		}
+		else if (matches === 1) {
+			recommendApp.recAnime[0].push(anime[series].title);
+		}
+	}
+	console.log("Recommended Anime:",recommendApp.recAnime);
 }
 
+//container for AJAX-ing all data from Popcorn Time
 recommendApp.getPopcornData = () => {
 	recommendApp.getMovies();
 	recommendApp.getShows();
 	recommendApp.getAnime();	
-}
+};
 
+//rounds a number to the nearest 0.25
 recommendApp.roundShowtime = (num) => {
 	return (Math.round(num*4)/4);
-}
+};
 
 //initialize
 recommendApp.init = () => {
+	recommendApp.reset();
 	recommendApp.getPopcornData();
 	recommendApp.getUserInput();
 };
 
+recommendApp.reset = () => {
+	//initialize arrays/objects for saving data
+	recommendApp.movies = {};
+	recommendApp.shows = {};
+	recommendApp.anime = {};
+
+	//recommended movies array
+	recommendApp.recMovies = [];
+	recommendApp.recMovies[0] = [];
+	recommendApp.recMovies[1] = [];
+	recommendApp.recMovies[2] = [];
+
+	//recommended anime array
+	recommendApp.recAnime = [];
+	recommendApp.recAnime[0] = [];
+	recommendApp.recAnime[1] = [];
+	recommendApp.recAnime[2] = [];
+
+};
 //document ready
 $(function() {
 	recommendApp.init();
